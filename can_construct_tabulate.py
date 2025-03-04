7a0474af2bfcee983eb3349f53d23f5fedd9ed0a
= <=>
2025-03-04 16:24:04 +0300

    Can construct using tabulate

--- can_construct_tabulate.py
+++ can_construct_tabulate.py
@@ -1,5 +1,14 @@
 from typing import List
 def can_construct_tabulate(target_str: str, available_strs: List[str])-> bool:
+    """_Determine if a target str can be formed by concatenating str from a given list_
+
+    Args:
+        target_str (str): _Word to construct_
+        available_strs (List[str]): _List of str to choose from_
+
+    Returns:
+        bool: _True if possible to concatenate str in list to form word else False_
+    """
     can_table: List[bool] = [False for _ in range(len(target_str) + 1)]
 
     for index in range(len(target_str)):
@@ -11,7 +20,7 @@ def can_construct_tabulate(target_str: str, available_strs: List[str])-> bool:
 
 def main()->None:
     target_str: str = 'abcdef'
-    words: List[str] = ['ab', 'abc', 'cd', 'ef', 'abcd']
+    words: List[str] = ['ab', 'abc', 'cd', 'e','f', 'abcd']
     is_p: bool = can_construct_tabulate(target_str=target_str, available_strs= words)
     print(f"Is it possible to form the word '{target_str}' from concatenating str in {words}? {is_p}")
 
