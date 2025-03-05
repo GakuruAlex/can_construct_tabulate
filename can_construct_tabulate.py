e7a86440c0d24ee5f753d564616887a1f53f7999
= <=>
2025-03-05 10:59:10 +0300

    Fixed the missing character in the middle alson resulting in true bug

--- can_construct_tabulate.py
+++ can_construct_tabulate.py
@@ -10,17 +10,17 @@ def can_construct_tabulate(target_str: str, available_strs: List[str])-> bool:
         bool: _True if possible to concatenate str in list to form word else False_
     """
     can_table: List[bool] = [False for _ in range(len(target_str) + 1)]
+    can_table[0] = True
 
     for index in range(len(target_str)):
         for char in available_strs:
-            current_slice: str = target_str[index: index + len(char)]
-            if current_slice == char:
+            if target_str[index: index + len(char)] == char and can_table[index]:
                 can_table[index + len(char)] = True
     return can_table[len(target_str)]
 
 def main()->None:
     target_str: str = 'abcdef'
-    words: List[str] = ['ab', 'abc', 'cd', 'e','f', 'abcd']
+    words: List[str] = ['ab', 'abc', 'cd', 'ef','abcd']
     is_p: bool = can_construct_tabulate(target_str=target_str, available_strs= words)
     print(f"Is it possible to form the word '{target_str}' from concatenating str in {words}? {is_p}")
 
